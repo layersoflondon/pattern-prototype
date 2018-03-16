497f05c9d47bfedee16fb9732ec357f03fd641ee
Ed Jones <ed@errorstudio.co.uk>
2018-03-16 09:27:48 +0000

    add mysql2 gem and fix up cap

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,6 +1,6 @@
 # Basic details
 set :application, 'pattern-prototype'
-set :repo_url, 'ssh://git@github.com:layersoflondon/pattern-prototype.git'
+set :repo_url, 'git@github.com:layersoflondon/pattern-prototype.git'
 set :primary_domain, "prototype.layersoflondon.org"
 # set :linked_dirs, fetch(:linked_dirs, []).push('app/downloads')
 
